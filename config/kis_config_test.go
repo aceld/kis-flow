25ef6d73c121e365378fd0ee454225cd5e591f9d
aceld <liudanbing@tal.com>
2023-12-31 15:26:53 +0800

    kis-flow v0.1

--- config/kis_config_test.go
+++ config/kis_config_test.go
@@ -1,30 +1,29 @@
-package test
+package config
 
 import (
 	"fmt"
 	"kis-flow/common"
-	"kis-flow/flow/kis_config"
 	"testing"
 )
 
 func TestNewFlowConfig(t *testing.T) {
 
-	flowFuncParams1 := flow.KisFlowFunctionParam{
+	flowFuncParams1 := KisFlowFunctionParam{
 		Fid: "funcId1",
-		Params: flow.FParam{
+		Params: FParam{
 			"flowSetFunParam1": "value1",
 			"flowSetFunParam2": "value2",
 		},
 	}
 
-	flowFuncParams2 := flow.KisFlowFunctionParam{
+	flowFuncParams2 := KisFlowFunctionParam{
 		Fid: "funcId2",
-		Params: flow.FParam{
+		Params: FParam{
 			"default": "value1",
 		},
 	}
 
-	myFlow1 := flow.NewFlowConfig("flowId", "flowName", 1)
+	myFlow1 := NewFlowConfig("flowId", "flowName", 1)
 	myFlow1.AppendFunctionConfig(flowFuncParams1)
 	myFlow1.AppendFunctionConfig(flowFuncParams2)
 
@@ -32,53 +31,53 @@ func TestNewFlowConfig(t *testing.T) {
 }
 
 func TestNewFuncConfig(t *testing.T) {
-	source := flow.KisSource{
+	source := KisSource{
 		Name: "公众号抖音商城户订单数据",
 		Must: []string{"order_id", "user_id"},
 	}
 
-	option := flow.KisFuncOption{
+	option := KisFuncOption{
 		Cid:          "connector_id",
 		RetryTimes:   3,
 		RetryDuriton: 300,
 
-		Params: flow.FParam{
+		Params: FParam{
 			"param1": "value1",
 			"param2": "value2",
 		},
 	}
 
-	myFunc1 := flow.NewFuncConfig("funcId", "funcName", "Save", &source, &option)
+	myFunc1 := NewFuncConfig("funcId", "funcName", "Save", &source, &option)
 
 	fmt.Printf("myFunc1: %+v\n", myFunc1)
 }
 
 func TestNewConnConfig(t *testing.T) {
 
-	source := flow.KisSource{
+	source := KisSource{
 		Name: "公众号抖音商城户订单数据",
 		Must: []string{"order_id", "user_id"},
 	}
 
-	option := flow.KisFuncOption{
+	option := KisFuncOption{
 		Cid:          "connector_id",
 		RetryTimes:   3,
 		RetryDuriton: 300,
 
-		Params: flow.FParam{
+		Params: FParam{
 			"param1": "value1",
 			"param2": "value2",
 		},
 	}
 
-	myFunc1 := flow.NewFuncConfig("funcId", "funcName", "Save", &source, &option)
+	myFunc1 := NewFuncConfig("funcId", "funcName", "Save", &source, &option)
 
-	connParams := flow.FParam{
+	connParams := FParam{
 		"param1": "value1",
 		"param2": "value2",
 	}
 
-	myConnector1 := flow.NewConnConfig("connectorId", "connectorName", "0.0.0.0:9987,0.0.0.0:9997", common.REDIS, "key", connParams)
+	myConnector1 := NewConnConfig("connectorId", "connectorName", "0.0.0.0:9987,0.0.0.0:9997", common.REDIS, "key", connParams)
 
 	if err := myConnector1.WithFunc(myFunc1); err != nil {
 		fmt.Printf("WithFunc err: %s\n", err.Error())
--- config/kis_flow_config.go
+++ config/kis_flow_config.go
@@ -1,4 +1,4 @@
-package flow
+package config
 
 import "kis-flow/common"
 
