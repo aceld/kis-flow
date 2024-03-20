4b7d213367959cbb9c52141f895de80788beec54
aceld <danbing.at@gmail.com>
2024-03-20 17:57:41 +0800

    change some test param, add test init()

--- test/kis_params_test.go
+++ test/kis_params_test.go
@@ -2,28 +2,16 @@ package test
 
 import (
 	"context"
-	"kis-flow/common"
 	"kis-flow/file"
 	"kis-flow/kis"
-	"kis-flow/test/caas"
-	"kis-flow/test/faas"
 	"testing"
 )
 
 func TestParams(t *testing.T) {
 	ctx := context.Background()
 
-	// 0. 注册Function 回调业务
-	kis.Pool().FaaS("funcName1", faas.FuncDemo1Handler)
-	kis.Pool().FaaS("funcName2", faas.FuncDemo2Handler)
-	kis.Pool().FaaS("funcName3", faas.FuncDemo3Handler)
-
-	// 0. 注册ConnectorInit 和 Connector 回调业务
-	kis.Pool().CaaSInit("ConnName1", caas.InitConnDemo1)
-	kis.Pool().CaaS("ConnName1", "funcName2", common.S, caas.CaasDemoHanler1)
-
 	// 1. 加载配置文件并构建Flow
-	if err := file.ConfigImportYaml("/Users/tal/gopath/src/kis-flow/test/load_conf/"); err != nil {
+	if err := file.ConfigImportYaml("/Users/Aceld/go/src/kis-flow/test/load_conf/"); err != nil {
 		panic(err)
 	}
 
