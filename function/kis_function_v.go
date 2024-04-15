f63ca63e57eae75701e0741f292e53fd7965d77a
刘丹冰 <danbing_at@163.com>
2024-04-15 11:17:47 +0800

    Revert "替换默认库为 slog"

--- function/kis_function_v.go
+++ function/kis_function_v.go
@@ -20,11 +20,11 @@ func NewKisFunctionV() kis.Function {
 }
 
 func (f *KisFunctionV) Call(ctx context.Context, flow kis.Flow) error {
-	log.Logger().Debug("KisFunctionV", "flow", flow)
+	log.Logger().DebugF("KisFunctionV, flow = %+v\n", flow)
 
 	// 通过KisPool 路由到具体的执行计算Function中
 	if err := kis.Pool().CallFunction(ctx, f.Config.FName, flow); err != nil {
-		log.Logger().ErrorX(ctx, "Function Called Error", "err", err)
+		log.Logger().ErrorFX(ctx, "Function Called Error err = %s\n", err)
 		return err
 	}
 
